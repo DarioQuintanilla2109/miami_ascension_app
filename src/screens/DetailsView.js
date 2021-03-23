d618bbe03e25ec09cb29dd5001720c1701c00ea2
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-03-23 00:15:48 -0400

    added dimensions for detailsview

--- src/screens/DetailsView.js
+++ src/screens/DetailsView.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import { View, Button, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Button, Text, StyleSheet, Image } from 'react-native'
 import DT from '../../darkTheme'
+import { FlatList } from 'react-native-gesture-handler'
+import { Dimensions } from 'react-native'
+
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
 
 function DetailsView({ navigation }) {
   const MyDarkTheme = DT()
 
+  const ARCHITECTURE = [{ image: require('../images/m1.jpg') }]
+
   return (
-    <View style={{ flex: 1, marginTop: 90 }}>
-      <Text style={{ color: MyDarkTheme.colors.text }}> DetailsView</Text>
-      <Button
-        onPress={() => navigation.navigate('MoreDetails')}
-        title='more details'
-      />
-    </View>
+    <>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          contentContainerStyle={styles.card}
+          data={ARCHITECTURE}
+          renderItem={({ item }) => (
+            <Image source={item.image} style={styles.imgStyle} />
+          )}
+        />
+      </View>
+    </>
   )
 }
+const styles = StyleSheet.create({
+  card: {
+    marginTop: windowHeight / 9,
+  },
+  wrapperStyle: {
+    padding: 0,
+    margin: 0,
+  },
+  imgStyle: {
+    height: 400,
+    maxWidth: '100%',
+    borderRadius: 25,
+  },
+})
 
 export default DetailsView
--- src/screens/MoreDetails.js
+++ src/screens/MoreDetails.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import { View, Button, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Button, Text, StyleSheet, Image } from 'react-native'
 import DT from '../../darkTheme'
+import { FlatGrid } from 'react-native-super-grid'
+import { FlatList } from 'react-native-gesture-handler'
 
 function MoreDetails({ navigation }) {
+  const [images, setImages] = useState([
+    { image: require('../images/m1.jpg') },
+    { image: require('../images/m3.jpg') },
+    { image: require('../images/m4.jpg') },
+    { image: require('../images/m5.jpg') },
+    { image: require('../images/m6.jpg') },
+    { image: require('../images/m7.jpg') },
+    { image: require('../images/m8.jpg') },
+  ])
   const MyDarkTheme = DT()
 
   return (
-    <View style={{ flex: 1, marginTop: 90 }}>
-      <Text style={{ color: MyDarkTheme.colors.text }}> More</Text>
-      <Button
-        title='Go to details view'
-        onPress={() => navigation.navigate('DetailsView')}
+    <>
+      <Text style={{ color: MyDarkTheme.colors.text }}> Miami</Text>
+      <FlatGrid
+        itemDimension={130}
+        data={images}
+        style={styles.gridView}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image
+              style={{
+                width: 400,
+                height: 400,
+                borderRadius: 10,
+              }}
+              source={item.image}
+            />
+          </View>
+        )}
       />
-    </View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  gridView: {
+    marginTop: 40,
+    flex: 1,
+  },
+  itemContainer: {
+    paddingLeft: 0,
+    paddingEnd: 20,
+  },
+})
 
 export default MoreDetails
